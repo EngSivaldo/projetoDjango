87029932334f0b3cb4b17d2282bdbeaf4e0dd0e2
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-10-20 19:20:23 -0300

    Criando o modelo do Filme - Adicionar tabelas no Banco de Dados

--- filme/models.py
+++ filme/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+LISTA_CATEGORIAS = (
+    ("ANALISES", "Análise"),
+    ("PROGRAMACAO", "Programação"),
+    ("APRESENTACAO", "Apresentação"),
+    ("OUTROS", "Outros"),
+
+
+
+
+)
+
+# Modelo Filme
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)  # título do filme
+    thumb = models.ImageField(upload_to='thumb_filmes')  # imagem de capa (miniatura)
+    descricao = models.TextField()  # descrição do filme
+    categoria = models.CharField(
+        max_length=50,
+        choices=LISTA_CATEGORIAS,
+        default='OUTROS'
+    )  # categoria com base na lista acima
+    visualizacoes = models.IntegerField(default=0)  # número de visualizações
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo  # nome legível no painel admin
