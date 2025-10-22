a7a5db7879f27beeeb41bc46233a33452c0d54c3
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-10-22 12:00:41 -0300

    Passando parâmetros para as páginas html - Context

--- filme/urls.py
+++ filme/urls.py
@@ -1,10 +1,11 @@
 # url, view, template
 from django.urls import path, include
 
-from .views import homepage
+from .views import homepage, homefilmes
 
 
 # 🔗 Rotas principais do site
 urlpatterns = [
     path('', homepage),  # inclui as rotas do app "filme"
+    path('filmes', homefilmes),
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+from filme.models import Filme
+
+
 # Create your views here.
 def homepage(request):
     return render(request, "homepage.html")
 
 
+# çriar uma nova views  (url- view - html- models)
+def homefilmes(request):
+    context = {}
+
+    lista_filmes = Filme.objects.all()  # chamar esta variavel listafilmes no homefilmes.html
+    context['lista_filmes'] = lista_filmes
+    return render(request, "homefilmes.html", context)
