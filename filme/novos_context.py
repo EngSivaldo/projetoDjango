d22c1f7c04dae86bb644d5d4c28ac6edaf403a27
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-10-27 11:57:14 -0300

    Edições Dinâmicas com Javascript e Filme em Destaque

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -3,7 +3,8 @@ from .models import Filme
 
 def lista_filmes_recentes(request):
     lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:10]
-    return {"lista_filmes_recentes": lista_filmes}
+    filme_destaque = lista_filmes[0]
+    return {"lista_filmes_recentes": lista_filmes, 'filme_destaque':filme_destaque }
 
 
 def lista_filmes_emalta(request):
@@ -11,6 +12,10 @@ def lista_filmes_emalta(request):
     return {'lista_filmes_emalta': lista_filmes}
 
 
+
+# def filme_destaque(request):
+#     filme = Filme.objects.all().order_by('-data_criacao')[0]
+#     return {"filme_destaque":filme}
 
 # apos criar os novos contextos, para conextar ao resto do projeto,
 # registra em settings.py em TEMPLATES/OPTIONS
